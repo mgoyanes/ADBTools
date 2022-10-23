986286fdf12fe6d14b9463e948224c7bd1b6fb7d
Miguel Goyanes <miguel.machado@wit-software.com>
2022-10-23 17:52:32 +0100

    Adds alternative method to detect current activity
    Google Pixel 4a with Android 12 uses topResumedActivity instead of mResumedActivity

--- src/main/kotlin/spock/adb/command/GetActivityCommand.kt
+++ src/main/kotlin/spock/adb/command/GetActivityCommand.kt
@@ -6,16 +6,38 @@ import spock.adb.ShellOutputReceiver
 import java.util.concurrent.TimeUnit
 
 class GetActivityCommand : Command<Any, String?> {
+
+    companion object {
+        private const val GET_TOP_ACTIVITY_COMMAND = "dumpsys activity | grep -E \"mResumedActivity|topResumedActivity\""
+        private const val ACTIVITY_PREFIX_DELIMITER = "."
+        private val extractAppRegex = Regex("(A=|I=|u0\\s)([a-zA-Z.]+)")
+        private val extractActivityRegex = Regex("(u0\\s[a-zA-Z.]+/)([a-zA-Z.]+)")
+    }
+
+
     override fun execute(p: Any, project: Project, device: IDevice): String? {
         val shellOutputReceiver = ShellOutputReceiver()
         device.executeShellCommand(
-            "dumpsys activity activities | grep mResumedActivity",
+            GET_TOP_ACTIVITY_COMMAND,
             shellOutputReceiver,
             15L,
             TimeUnit.SECONDS
         )
-        return shellOutputReceiver.toString().split(" ").find { it.contains("/") }
-            ?.replace("/.", ".")
-            ?.replace(Regex(".+/"), "")
+
+        val appPackage = getAppPackage(shellOutputReceiver.toString())
+        appPackage ?: return null
+
+        val topActivity = getActivityName(shellOutputReceiver.toString())
+        topActivity ?: return null
+
+
+        return when {
+            topActivity.startsWith(ACTIVITY_PREFIX_DELIMITER) -> "$appPackage$topActivity"
+            else -> topActivity
+        }
     }
+
+    private fun getActivityName(bulkAppData: String) = extractActivityRegex.find(bulkAppData)?.groups?.lastOrNull()?.value
+
+    private fun getAppPackage(bulkAppData: String) = extractAppRegex.find(bulkAppData)?.groups?.lastOrNull()?.value
 }
