635c971c4244b43cfb38e0dcf8cac400681d44dc
Miguel Goyanes <miguel.machado@vodafone.com>
2024-02-25 10:16:34 +0000

    Update how facets are obtained. Supports newer versions of AS.

--- src/main/kotlin/spock/adb/command/GetPackageNameCommand.kt
+++ src/main/kotlin/spock/adb/command/GetPackageNameCommand.kt
@@ -2,15 +2,19 @@ package spock.adb.command
 
 import com.android.ddmlib.IDevice
 import com.android.tools.idea.projectsystem.getModuleSystem
+import com.intellij.facet.ProjectFacetManager
 import com.intellij.openapi.project.Project
-import org.jetbrains.android.util.AndroidUtils
+import org.jetbrains.android.facet.AndroidFacet
 
 class GetPackageNameCommand : Command<Any, String?> {
-    override fun execute(p: Any, project: Project, device: IDevice): String? =
-        AndroidUtils
-            .getApplicationFacets(project)
+    override fun execute(p: Any, project: Project, device: IDevice): String? {
+        val facets = ProjectFacetManager
+            .getInstance(project)
+            .getFacets(AndroidFacet.ID)
+        return facets
             .firstOrNull()
             ?.module
             ?.getModuleSystem()
             ?.getPackageName()
+    }
 }
